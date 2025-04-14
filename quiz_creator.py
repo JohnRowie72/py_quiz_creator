b4ad47e362abc2b9926971cca6ffb00b1235bbd7
John Rowie Maristela <maristelajohnrowie@gmail.com>
2025-04-14 23:40:08 +0800

    pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,9 @@
+# import necessary libraries for file handling
+# define a function that formats the questions, options, and correct answer
+# and into a structured block to be saved in a text file
+# define a function to append the formatted question block to a text file
+# define a function to validate the correct answer input
+# keeps asking until a valid input is provided
+# define the main logic to create a quiz
+# loop to repeatedly ask for quiz questions and store them in a file
+# run the main function if the script is executed directly
